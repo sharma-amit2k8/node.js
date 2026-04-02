118bb35cc2cc80d319de4bb3353c89e7dd20091a
Kumar <sharma.amit2k8@gmail.com>
2026-04-02 15:15:20 +0530

    updated localhost to weatherstack url

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -9,18 +9,18 @@ e.preventDefault();
 
 if(search.value){
     msg1.textContent = 'Loading...'
-fetch('http://localhost:1200/weather?address=' + search.value).then((response) => {
+fetch('https://api.weatherstack.com/current?access_key=0683370f14c91d912b3a115e4760bd89&query=' + search.value).then((response) => {
     response.json().then((data) => {
         // console.log({data})
         if(data.error){
             msg1.textContent = ''
-            msg2.textContent = 'Error ' + data.error
-            console.log(data.error)
+            msg2.textContent = 'Error ' + data.error.info
+            // console.log(data.error)
             }
         else{
-            msg1.textContent = data.address + ' : '  + data.temp
+            msg1.textContent = data.location.name + ' : '  + data.current.temperature
             msg2.textContent = ''
-            console.log(data.address, ' : ' ,data.temp)
+            // console.log(data.address, ' : ' ,data.temp)
         }  
         
     })
